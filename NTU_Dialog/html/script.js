04694454055f19103dca94e298a1a9a83059965d
Salisu Wada <wadasalisu@gmail.com>
2018-04-25 16:12:05 +0100

    Florian commit

---
+++ NTU_Dialog/html/script.js
@@ -0,0 +1,21 @@
+// global session
+var session = new QiSession(function(session) {
+                // document.getElementById('typed').innerHTML = "Connection esterblished!";
+              }, function() {
+                // document.getElementById('typed').innerHTML = "Could not connect to the robot";
+              });
+
+   
+session.service("ALMemory").then(function(ALMemory) {
+  // document.getElementById('typed').innerHTML = "ALMemory proxy subscription successful!";  
+  ALMemory.getData('keyword').then(function(keyword){
+        
+        new Typed('#typed', {
+          strings: [keyword],
+          typeSpeed: 30,
+          fadeOut: true,
+        });
+
+  });
+  
+});
